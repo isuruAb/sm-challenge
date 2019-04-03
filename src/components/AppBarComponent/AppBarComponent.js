250a0ba8274390ac01805bae1f591521082c3601
isuruAb <vpisabeywardana@gmail.com>
2019-04-03 16:55:31 +0800

    Fixes #2 : Added AppBar and routings to main dashboard

---
+++ src/components/AppBarComponent/AppBarComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+class AppBarComponent extends React.Component {
+    render() {
+        return (
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" >
+                        SMove Challenge
+  </Typography>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
+
+
+export default AppBarComponent;
